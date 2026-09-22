cfa4ded1a5cfedccb3d9ae0f76e370f88dce1a4d
Keerthy Rangan <keerthyrangan1@gmail.com>
2026-09-21 20:40:51 -0400

    Remove unnecessary lines from find_homologs.sh

--- find_homologs.sh
+++ find_homologs.sh
@@ -7,9 +7,3 @@ tblastn -query "$1" -subject "$2" -outfmt 6 > blast.txt
 awk 'NR==FNR {len[$1]=$2; next} $3 > 30 && $4>len[$1]' query_len.txt blast.txt > "$3"
 
 wc -l $3
-~
-~                                                                                                                    
-~                                                                                                  
-wq
-
-~                                                                                                                    
